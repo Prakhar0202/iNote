4260633198be866c5db0db5cddb8c35f173735c1
dev-prakhar <prakhar02ver@gmail.com>
2024-07-30 12:37:03 +0530

    initial

---
+++ backend_inote/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = ()=> {
+    mongoose.connect(mongoURI)
+}
+
+module.exports = connectToMongo;
---
+++ backend_inote/index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require('./db');
+connectToMongo();
+
+const express = require('express')
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example listening on http://localhost: ${port}`)
+})
