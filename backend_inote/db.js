df6377ad49b874a28b71f6a58f1459b37138b975
dev-prakhar <prakhar02ver@gmail.com>
2024-07-30 13:47:30 +0530

    initial

--- backend_inote/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoURI = "mongodb://localhost:27017/";
-
-const connectToMongo = ()=> {
-    mongoose.connect(mongoURI)
-}
-
-module.exports = connectToMongo;
