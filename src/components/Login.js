21d8f3ba49dfaad4c639de2f0ecacc54b801d080
dev-prakhar <prakhar02ver@gmail.com>
2024-08-06 14:20:50 +0530

    added login functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,23 +1,75 @@
-import React from 'react'
+import React, { useState } from "react";
+import {useNavigate } from "react-router-dom";
 
-const login = () => {
+const Login = () => {
+  const [credentials, setCredentials] = useState({ email: "", password: "" });
+// let navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(`http://localhost:5000/api/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        email: credentials.email,
+        password: credentials.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    console.log(json);
+    if(json.success)
+    {
+        // localStorage.setItem('token',json.authtoken);
+        // navigate('/')
+    }
+    else{
+        alert("Invalid Credentials")
+    }
+  };
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
   return (
     <div>
-       <form>
-  <div className="mb-3">
-    <label for="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label for="password" className="form-label">Password</label>
-    <input type="password" className="form-control" id="password" name="password"/>
-  </div>
-  
-  <button type="submit" className="btn btn-primary">Login</button>
-</form>
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+            value={credentials.email}
+            onChange={onChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            value={credentials.password}
+            onChange={onChange}
+          />
+        </div>
 
-export default login
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const signup = () => {
+const Signup = () => {
+ 
   return (
     <div>
-     <form>
-  <div className="mb-3">
-    <label for="email" className="form-label">Email address</label>
-    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp"/>
-    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div className="mb-3">
-    <label for="password" className="form-label">Password</label>
-    <input type="password" className="form-control" id="password" name="password"/>
-  </div>
-  
-  <button type="submit" className="btn btn-primary" >Signup</button>
-</form>
-    </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="email"
+            aria-describedby="emailHelp"
+            // value={credentials.email}
+            // onChange={onChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="password"
+            // value={credentials.password}
+            // onChange={onChange}
 
-export default signup
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Signup
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Signup;
